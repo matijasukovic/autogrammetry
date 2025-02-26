c154073c6a3761504f1f27e15ec8404ac49412fb
matijasukovic <suksa186@gmail.com>
2025-02-26 22:35:21 +0100

    feat: add Camera class

--- classes/camera.py
+++ classes/camera.py
@@ -1,20 +1,25 @@
 from picamera2 import Picamera2, Preview
 from libcamera import controls
 import cv2
+import os
+from PIL import Image
 
 class Camera:
     def __init__(self):
         self.picamera = Picamera2()
 
-        preview_config = self.picamera.create_preview_configuration(
-            main={"size": (1920, 1920)}
+        self.still_config = self.picamera.create_still_configuration(
+            raw={"size": (4056, 3040)},
         )
         
-        self.picamera.configure(preview_config)
-        # self.picamera.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+        self.picamera.configure(self.still_config)
+
+        self.picamera.set_controls({"ExposureTime": 40000, "AnalogueGain": 1.0, "Contrast": 1.2, "Sharpness": 2})
+        print(self.picamera.camera_controls)
+
         self.picamera.start()
 
-        self.savedImage_filename = 'test'
+        self.savedImage_filename = 'top'
         self.savedImage_index = 0
         self.savedImage_extension = '.png'
 
@@ -22,9 +27,21 @@ class Camera:
         img = self.picamera.capture_array()
         return cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
     
-    def captureAndSave(self):
-        save_path = self.savedImage_filename + '_' + str(self.savedImage_index) + self.savedImage_extension
-        self.picamera.capture_file(save_path)
-        print('saved as: ' ,save_path)
+    def captureAndSave(self, output_dir='.'):
+        filename = self.savedImage_filename + '_' + str(self.savedImage_index) + self.savedImage_extension
+        save_path = os.path.join(output_dir, filename)
 
+        self.picamera.capture_file(save_path)
+        print('saved as: ' ,save_path) 
+
+        self.savedImage_index += 1
+
+    def saveImage(self, image, output_dir='.'):
+        filename = self.savedImage_filename + '_' + str(self.savedImage_index) + self.savedImage_extension
+        save_path = os.path.join(output_dir, filename)
+
+        img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(save_path, image)
+
+        print('saved as: ' ,save_path) 
         self.savedImage_index += 1
