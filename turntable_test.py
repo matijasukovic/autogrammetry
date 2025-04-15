534638fedc24d7d13e4cfecc17dca48e6801d33c
matijasukovic <suksa186@gmail.com>
2025-04-15 22:33:25 +0200

    feat: update turntable_test.py

--- turntable_test.py
+++ turntable_test.py
@@ -1,38 +1,28 @@
-from gpiozero import OutputDevice
-from time import sleep
+from classes.turntable import Turntable
+import time
 
-IN1 = OutputDevice(14)
-IN2 = OutputDevice(15)
-IN3 = OutputDevice(18)
-IN4 = OutputDevice(23)
+turntable = Turntable()
 
-step_sequence = [
-    [1, 0, 0, 0],
-    [1, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 1, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 1],
-    [0, 0, 0, 1],
-    [1, 0, 0, 1]
-]
-
-def set_step(w1, w2, w3, w4):
-    IN1.value = w1
-    IN2.value = w2
-    IN3.value = w3
-    IN4.value = w4
-
-def step_motor(steps, direction=1, delay=0.001):
-    for _ in range(steps):
-        for step in (step_sequence if direction > 0 else reversed(step_sequence)):
-            set_step(*step)
-            sleep(delay)
-
-try:
+def main():
     while True:
-        steps = int(input("Enter number of steps (eg. 3072 for one full revolution): "))
-        direction = int(input("Enter direction (1 or -1): "))
-        step_motor(steps, direction)
-except KeyboardInterrupt:
-    print("Program stopped by user")
+        cmd = input("Enter 'start' to run motor, 'stop' to halt, or 'exit' to quit: ").strip().lower()
+        if cmd == "start":
+            turntable.startMotorContinuous()
+        elif cmd == "stop":
+            turntable.stopMotor()
+        elif cmd == "exit":
+            turntable.stopMotor()
+            break
+        else:
+            print("Invalid command. Use 'start', 'stop', or 'exit'.")
+
+if __name__ == '__main__':
+    main()
+
+# try:
+#     while True:
+#         steps = int(input("Enter number of steps (eg. 3072 for one full revolution): "))
+#         direction = int(input("Enter direction (1 or -1): "))
+#         turntable.step_motor(steps, direction)
+# except KeyboardInterrupt:
+#     print("Program stopped by user")
